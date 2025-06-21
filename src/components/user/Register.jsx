ed1caab26245eaea18b9bb79e9287d94caa06d4f
Rajeev0805 <rajeevgupta0805@gmail.com>
2025-06-21 12:28:11 +0530

    Refactor Register component for improved readability and maintainability

--- src/components/user/Register.jsx
+++ src/components/user/Register.jsx
@@ -4,29 +4,32 @@ import { CgRename } from "react-icons/cg";
 import { MdAlternateEmail } from "react-icons/md";
 import { RiLockPasswordLine } from "react-icons/ri";
 import { MdEventRepeat } from "react-icons/md";
-import { FaRepeat } from 'react-icons/fa6';
 import {validatePassword} from "val-pass"
 import toast from 'react-hot-toast';
 
 
 const Register = () => {
-  const [formData,setFormData]=useState({ })
-  const [repeatPassword,setRepeatPassword]=useState(true)
-  const [errorMessage,seterrorMessage] = useState("")
-  const handlePassword=(e)=>{
-    let {value} = e.target
-    formData.password!=value?setRepeatPassword(false):setRepeatPassword(true)
-    value==""&&setRepeatPassword(true)
-  }
+  const [formData,setFormData]=useState({
+    userName:"",
+    password:"",
+    name:"",
+    email:""
+  })
+
+  const [matched,setMatched]=useState(true)
+  const[errorMessage,setErrorMessage]=useState("")
+
   const handelChange=e=>{
     let {name,value}=e.target
     if(name=="password"){
       let {validateAll,getAllValidationErrorMessage}=validatePassword(value,8)
-      validateAll()?seterrorMessage(""):seterrorMessage(getAllValidationErrorMessage())
-      value==""&&seterrorMessage("")
+      // console.log(getAllValidationErrorMessage());
+      validateAll()?setErrorMessage(""):setErrorMessage(getAllValidationErrorMessage())
+      value==""&&setErrorMessage("")
     }
     setFormData((preVal)=>({...preVal,[name]:value}))
   }
+
   const handelSubmit=e=>{
     e.preventDefault()
     let {name,userName,password,email}=formData
@@ -40,14 +43,23 @@ const Register = () => {
     }
     if(!matched){
       toast.error("passsword and confirm password did not match")
-      return
+  return
     }
-     console.log(formData);
-    
+ console.log(formData);
+ 
+   
   }
+
+  const handelCheckPassword=(e)=>{
+    let {value} =e.target
+    formData.password!=value?setMatched(false):setMatched(true)
+   value==""&&setMatched(true)
+  }
+
+  
   return (
-    <div className='bg-[#3a922ca1] size-full flex justify-center items-center'>
-      <form action="" className='w-1/2 h-[90%]  rounded-2xl bg-white shadow-2xl flex  items-center flex-col gap-4 px-[80px] py-10 max-sm:w-[90%]' onSubmit={handelSubmit}>
+    <div className='bg-[#1edeb8a5] size-full flex justify-center items-center'>
+      <form action="" className='w-1/2 h-[90%]  rounded-3xl bg-white shadow-2xl flex  items-center flex-col gap-4 px-[80px] py-10 max-sm:w-[90%] overflow-scroll' onSubmit={handelSubmit}>
         <div className='font-bold w-full flex justify-center items-center'>
           <h1 className='text-3xl max-lg:text-sm'>Registration Form</h1>
         </div>
@@ -74,19 +86,18 @@ const Register = () => {
             <span><RiLockPasswordLine /></span>
         </div>
 
-         <div className={errorMessage?'w-full flex justify-center items-center px-3 rounded-sm':'hidden'}>
+        <div className={errorMessage?'w-full flex justify-center items-center px-3 rounded-sm':'hidden'}>
           <span className='text-red-700'>*{errorMessage}</span>
         </div>
-
         
-        <div className={`border-2  w-full flex justify-center items-center px-3 rounded-sm ${repeatPassword?`border-black`:`bg-red-600`}`}>
-          <input type="password" name="repeatPassword" placeholder='Re-type passoword' className='w-full outline-none px-4 h-10' onChange={handlePassword}/>
-            <span><FaRepeat /></span>
+        <div className={`border-2  w-full flex justify-center items-center px-3 rounded-sm ${matched?'border-black':'bg-red-700'}`} >
+          <input type="password"  placeholder='Re-type passoword' className='w-full outline-none px-4 h-10'  onChange={handelCheckPassword}/>
+            <span><MdEventRepeat /></span>
         </div>
 
         
-        <div className='border-2  w-full flex justify-center items-center px-3 rounded-sm bg-black hover:bg-[#3a922ca1] active:bg-lime-500 active:scale-[0.95]'>
-         <button className='h-10 max-w-full text-white text-md tracking-widest'>Click</button>
+        <div className='border-2  w-full flex justify-center items-center px-3 rounded-sm bg-black hover:bg-[#73ebeb] active:bg-cyan-500 active:scale-[0.985]'>
+         <button className='h-10 text-white font-bold text-md tracking-widest w-full'>Click</button>
         </div>
       </form>
     </div>
