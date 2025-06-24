a27bc2ece1c17ff5e38075dbdea48f264a446a9c
Rajeev0805 <rajeevgupta0805@gmail.com>
2025-06-24 17:58:11 +0530

    Add Main layout with navigation and routing; implement Home, About, AddBill, and FilterBill components

---
+++ src/components/user/main/filterBill/FilterBill.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FilterBill = () => {
+  return (
+    <div>FilterBill</div>
+  )
+}
+
+export default FilterBill
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -2,6 +2,10 @@ import { createBrowserRouter } from "react-router-dom";
 import Register from "../components/user/Register";
 import Login from "../components/user/Login";
 import Main from "../components/user/main/Main";
+import About from "../components/user/main/about/About";
+import AddBill from "../components/user/main/addBill/AddBill";
+import FilterBill from "../components/user/main/filterBill/FilterBill";
+import Home from "../components/user/main/home/Home";
 
 let routes=createBrowserRouter([
     {
@@ -14,7 +18,25 @@ let routes=createBrowserRouter([
     },
     {
         path:"/Home",
-        element : <Main/>
+        element : <Main/>,
+         children:[
+            {
+                index:true,
+                element:<Home/>
+            },
+            {
+                path:"About",
+                element:<About></About>
+            },
+            {
+                path:"AddBills",
+                element:<AddBill></AddBill>
+            },
+            {
+                path:"FilterBills",
+                element:<FilterBill></FilterBill>
+            }
+        ]
     }
 ])
 
